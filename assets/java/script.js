e09dc9531b7cc280855019fa73b583c078bbe336
Malcolm Franklin <mac.mac5@yahoo.com>
2024-05-26 18:48:07 -0600

    made page responsive for mobile

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,9 +1,8 @@
-const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.nav-menu');
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburger = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('.nav-menu');
 
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    navMenu.classList.toggle('active');
-})
-
-addEventListener
+    hamburger.addEventListener('click', function () {
+        navMenu.classList.toggle('active');
+    });
+});
